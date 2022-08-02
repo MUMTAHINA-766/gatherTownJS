6ff8b45687d00fa28f93cc4a9afbd5dcba14f532
MUMTAHINA-766 <64628178+MUMTAHINA-766@users.noreply.github.com>
2022-08-02 08:09:24 +0600

    Create exercise10.js

---
+++ exercise10.js
@@ -0,0 +1,13 @@
+//exercise-10########
+ //q-1
+  function multiplying(a,b)
+  {
+    console.log(a*b);
+  } 
+  multiplying(3,5)
+ //q-2
+  function greet(name)
+    {
+      console.log("Hello",name);
+    }
+  greet("Parvin");
